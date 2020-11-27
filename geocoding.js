22b1f771e8e41f77f7d60ca506adf44ef2ec2710
Sven_Maier <sven.maier132000@gmail.com>
2020-11-27 09:17:13 +0100

    Adding working file geocoding.js

---
+++ geocoding.js
@@ -0,0 +1,2 @@
+//Javascript file to get a place from geo coords getplace(float lat, float lng) -> bsp. location2geo github
+// and to get the position getposition() -> bsp. geo2location auf github 
