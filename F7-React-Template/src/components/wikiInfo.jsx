74b3e0aa46de55509a811b3b9694ce8716809b93
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-12-01 15:45:30 +0100

    Wikipedia-Fetch-Funktion

--- F7-React-Template/src/components/wikiInfo.jsx
+++ F7-React-Template/src/components/wikiInfo.jsx
@@ -18,5 +18,122 @@ class WikiInfo extends React.Component {
             </Card>
         );
     }
+	
+	// Writes the error information to the state
+	function handleError(responseStatus, response) {
+		this.setState(
+			{
+				"title": "Error :(",
+				"subtitle": "Error-Code: " + responseStatus,
+				"content": response
+			}
+		);
+	}
+	
+	async function fetchWikipedia(locationName) {
+		// Prepare String (Exchange spaces with '%20' and so on...)
+		var locationNamePrepared = locationName.replace(' ', '%20');
+		// Enter "Loading" Information in state
+		this.setState(
+			{
+				"title": "Loading...",
+				"subtitle": "Loading...",
+				"content": "Loading..."
+			}
+		);
+		// Fetch Wikipedia API
+		// Step 1: Get title and page URL
+		var requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+		}
+		var pageID = "";
+		var pageTitle = "";
+		var subtitle = "";
+		var content = "";
+		var wikiURL = "http://de.wikipedia.org/w/api.php?action=query&list=search&rsearch=" + locationNamePrepared + "&format=json";
+		
+		fetch(wikiURL, requestOptions)
+			.then( response => {
+				responseStatus = response.status;
+				return response.json();
+			})
+			.then( json => {
+				switch(responseStatus) {
+					//Everything is fine
+					case 200:
+						pageID = json.query.results.search[0].pageid;
+						pageTitle = json.query.results.search[0].title;
+						subtitle = json.query.results.search[0].snippet;
+						break;
+					//Something went wrong
+					default:
+						handleError(responseStatus, response);
+						return;
+				}
+			})
+		//Step 2: Get description and small extract
+		wikiURL = "http://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=" + pageID;
+		fetch(wikiURL, requestOptions)
+			.then( response => {
+				responseStatus = response.status;
+				return response.json();
+			})
+			.then( json => {
+				switch(responseStatus) {
+					case 200:
+						content = json.query.pages[pageID].extract;
+						break;
+					default:
+						handleError(responseStatus, response);
+						return;
+				}
+			})
+		//Old solution, might be a little bit out of date but is more readable	
+		// Should work definitely as I use this style in other projects as well
+		/*let response = await fetch(wikiURL, requestOptions);
+		let responseData = await response.json();
+		if (!responseData) {
+			// TODO: Note down that there was nothing
+			return;
+		}
+		let results = responseData.query;
+		if (!results) {
+			//TODO: Note down that there was no content
+			return;
+		}
+		//First entry (most fitting one)
+		results = results.search[0];
+		// Extract information
+		var pageID = results.pageid;
+		var pageTitle = results.title;
+		// Step 2: Get description and subtitle
+		var subtitle = results.snippet;
+		// Request page description
+		wikiURL = "http://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=" + pageID;
+		let responseDescription = await fetch(wikiURL, requestOptions);
+		let responseDataDescription = await responseDescription.json();
+		if(!responseDataDescription) {
+			//TODO: Note down that there was nothing
+			return;
+		}
+		let resultsDescription = responseDataDescription.query;
+		if(!resultsDescription) {
+			//TODO: Note down that there was no content
+			return;
+		}
+		resultsDescription = resultsDescription.pages[pageID];
+		var content = resultsDescription.extract;
+		// Error handling => TODO*/
+		// Write results to state
+		this.setState(
+			{
+				"title": pageTitle,
+				"subtitle": subtitle,
+				"content": content
+			}
+		);
+		return;
+	}
 }
 export default WikiInfo;
