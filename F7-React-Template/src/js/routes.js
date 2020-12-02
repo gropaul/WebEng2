141d325f603df462606555a6dcfe86321744d18e
danielrichter19 <50749086+danielrichter19@users.noreply.github.com>
2020-12-02 18:39:59 +0100

    Add files via upload
    Add F7_Template

---
+++ F7-React-Template/src/js/routes.js
@@ -0,0 +1,25 @@
+import AboutPage from '../pages/about.jsx';
+import MAP from '../pages/map.jsx';
+import InfoPage from '../pages/infos.jsx'
+import NotFoundPage from '../pages/404.jsx';
+
+var routes = [
+  {
+    path: '/',
+    component: MAP,
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+  },
+  {
+    path: '/infos/',
+    component: InfoPage,
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
+
+export default routes;
---
+++ F7-React-Template/src/pages/map.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Navbar } from 'framework7-react';
+import '../css/map.css';
+
+class Map extends Component {
+
+	render() {
+
+		return (
+			<div>
+				<Navbar title="Karte" />
+				<LeafletMap
+					center={[48.137, 11.575]}
+					zoom={7}
+
+				>
+					<TileLayer
+						url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					/>
+				</LeafletMap>
+
+			</div>
+		);
+	}
+}
+
+export default Map
