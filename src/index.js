9b63c0e8116132211b514b1941b53dd97fee459f
danielrichter19 <50749086+danielrichter19@users.noreply.github.com>
2020-12-02 19:05:48 +0100

    Add files via upload
    Added MapComponent Maps and both Markers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import TaskList from './components/TaskList';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import counter from './reducers/index';
@@ -13,9 +12,7 @@ console.log(store.getState());
 ReactDOM.render(
   <Provider store={store}>
     <div>
-      <h1 className="App">React Redux Training</h1>
       <App />
-      <TaskList />
     </div>
   </Provider>,
   document.getElementById('root')
