88781333db0386dcd2b31d9354bf6afd2029cb68
Paul Groß <paul.riedlingen@gmail.com>
2020-12-15 11:10:13 +0100

    Add files via upload

---
+++ src/reverseGeocoding/geo2location.js
@@ -0,0 +1,15 @@
+
+function get_location(lng, lat) {
+	
+	fetch('https://nominatim.openstreetmap.org/reverse?format=json&lon=' + lng + '&lat=' + lat).
+			then(function(response) {
+					return response.json();
+			}).
+			then(function(json) {
+
+				var data = json.address;
+
+				//console.log(data);
+				return data;
+			})			
+}
